f335881fd0d3922ead144b143bba4f2d44d1a52f
khaitan207ct28480 <tan.207ct28480@vanlanguni.vn>
2024-07-16 11:01:04 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster207.waieqww.mongodb.net',
+    DB_USER: 'tan207ct28480',
+    DB_PASS: 'VLU12112002',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '207ct28480',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'nguyenlekhaitan90417@gmail.com', // gmail service
+    EMAIL_PASS: 'opzu zzkj odgo kgbq'
+  };
+  module.exports = MyConstants;
